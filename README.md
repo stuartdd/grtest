818939c99b158202247ec77b9046e5a578461473
stuartdd <stuart.d.davies@btinternet.com>
2022-08-31 01:13:32 +0100

    add icon

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # grtest
+
+## Bundle creation:
+
+```bash
+fyne bundle -o bundle.go go-logo-blue.png
+```
+
+Use -a to append to existing -o file.
--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
@@ -10,6 +12,13 @@ import (
 func main() {
 	a := app.New()
 	mainWindow := a.NewWindow("Hello")
+	mainWindow.SetCloseIntercept(func() {
+		fmt.Println("Closed 0")
+		mainWindow.Close()
+	})
+	mainWindow.SetMaster()
+	mainWindow.SetIcon(resourceGoLogoBluePng)
+
 	mainWindow.Resize(fyne.Size{Width: 300, Height: 200})
 
 	hello := widget.NewLabel("Hello Fyne!")
