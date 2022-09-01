3176bbb76125713c0d964eb4bdf1421dd4a916a6
stuartdd <stuart.d.davies@btinternet.com>
2022-09-01 01:18:59 +0100

    Movers

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"time"
 
 	"fyne.io/fyne/v2"
@@ -10,12 +9,11 @@ import (
 	"fyne.io/fyne/v2/container"
 )
 
-var runMainLoop = true
-
 type Movable interface {
-	// Move()
 	Init()
-	Update(time float32)
+	Update(float32)
+	AdjustSpeed(float32, float32)
+	getCanvasObject() fyne.CanvasObject
 }
 
 type MoverImage struct {
@@ -24,8 +22,8 @@ type MoverImage struct {
 	posx   float32
 	posy   float32
 
-	iconSize fyne.Size
-	icon     *canvas.Image
+	imageSize fyne.Size
+	image     *canvas.Image
 }
 
 type ControllerLayout struct {
@@ -33,18 +31,36 @@ type ControllerLayout struct {
 	movers []Movable
 }
 
+func NewControllerContainer(width, height float32) *ControllerLayout {
+	return &ControllerLayout{size: fyne.Size{Width: width, Height: height}, movers: make([]Movable, 0)}
+}
+
+func NewMoverImage(x, y, w, h float32, image *canvas.Image) *MoverImage {
+	return &MoverImage{imageSize: fyne.Size{Width: w, Height: h}, image: image, posx: x, posy: y, speedx: 0, speedy: 0}
+}
+
 func (l *MoverImage) Update(time float32) {
 	px := l.posx + (l.speedx * time)
 	py := l.posy + (l.speedy * time)
 	if (px != l.posx) || (py != l.posy) {
-		l.icon.Move(fyne.Position{X: px, Y: py})
+		l.image.Move(fyne.Position{X: px, Y: py})
 		l.posx = px
 		l.posy = py
 	}
 }
 
+func (l *MoverImage) getCanvasObject() fyne.CanvasObject {
+	return l.image
+}
+
 func (l *MoverImage) Init() {
-	l.icon.Resize(l.iconSize)
+	l.image.Resize(l.imageSize)
+	l.image.FillMode = canvas.ImageFillOriginal
+}
+
+func (l *MoverImage) AdjustSpeed(x, y float32) {
+	l.speedx = l.speedx + x
+	l.speedy = l.speedy + y
 }
 
 func (l *ControllerLayout) Layout(objects []fyne.CanvasObject, size fyne.Size) {
@@ -63,7 +79,8 @@ func (l *ControllerLayout) Init() {
 	}
 }
 
-func (l *ControllerLayout) Add(m Movable) {
+func (l *ControllerLayout) Add(m Movable, container *fyne.Container) {
+	container.Add(m.getCanvasObject())
 	l.movers = append(l.movers, m)
 }
 
@@ -72,35 +89,38 @@ func (l *ControllerLayout) MinSize(objects []fyne.CanvasObject) fyne.Size {
 }
 
 func main() {
-	image := canvas.NewImageFromResource(Lander_Png)
-	image.FillMode = canvas.ImageFillOriginal
-
-	layout := &ControllerLayout{size: fyne.Size{Width: 500, Height: 300}, movers: make([]Movable, 0)}
-	layout.Add(&MoverImage{iconSize: fyne.Size{Width: 40, Height: 40}, icon: image, posx: 0, posy: 0, speedx: 10, speedy: 10})
+	moverImage1 := NewMoverImage(0, 0, 40, 40, canvas.NewImageFromResource(Lander_Png))
+	moverImage1.AdjustSpeed(10, 10)
+	moverImage2 := NewMoverImage(0, 0, 20, 20, canvas.NewImageFromResource(Lander_Png))
+	moverImage2.AdjustSpeed(5, 5)
 
 	a := app.New()
 	mainWindow := a.NewWindow("Hello")
 	mainWindow.SetCloseIntercept(func() {
-		runMainLoop = false
-		time.Sleep(time.Millisecond * 500)
 		mainWindow.Close()
 	})
 	mainWindow.SetMaster()
 	mainWindow.SetIcon(GoLogo_Png)
 
-	cvs := container.New(layout, image)
-	mainWindow.SetContent(cvs)
-	go func() {
-		layout.Init()
-		timeOffset := time.Now().UnixMilli()
-		for runMainLoop {
-			time.Sleep(time.Millisecond * 100)
-			layout.Update(float32(time.Now().UnixMilli()-timeOffset) / 1000.0)
-			cvs.Refresh()
-			timeOffset = time.Now().UnixMilli()
-		}
-		fmt.Println("CanRun No")
-	}()
+	controller := NewControllerContainer(500, 500)
+	container := container.New(controller)
+	controller.Add(moverImage1, container)
+	controller.Add(moverImage2, container)
 
+	mainWindow.SetContent(container)
+
+	controller.Init()
+	var ft float32 = 0
+	an := fyne.Animation{Duration: time.Duration(time.Second), RepeatCount: 1000000, Curve: fyne.AnimationLinear, Tick: func(f float32) {
+		if f == 1.0 {
+			ft = 0
+		} else {
+			controller.Update(f - ft)
+			container.Refresh()
+			ft = f
+		}
+	}}
+	an.Start()
 	mainWindow.ShowAndRun()
+	an.Stop()
 }
